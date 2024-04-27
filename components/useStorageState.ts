e9852c7b643e7b94d0c5e77d63a3a7acdc795bbb
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-04-27 14:01:34 +0330

    feat: add useStorageState from expo docs

---
+++ components/useStorageState.ts
@@ -0,0 +1,42 @@
+import * as SecureStore from "expo-secure-store";
+import * as React from "react";
+
+type UseStateHook<T> = [[boolean, T | null], (value: T | null) => void];
+
+function useAsyncState<T>(initialValue: [boolean, T | null] = [true, null]): UseStateHook<T> {
+  return React.useReducer(
+    (state: [boolean, T | null], action: T | null = null): [boolean, T | null] => [false, action],
+    initialValue,
+  ) as UseStateHook<T>;
+}
+
+export async function setStorageItemAsync(key: string, value: string | null) {
+  if (value == null) {
+    await SecureStore.deleteItemAsync(key);
+  } else {
+    await SecureStore.setItemAsync(key, value);
+  }
+}
+
+export function useStorageState(key: string): UseStateHook<string> {
+  // Public
+  const [state, setState] = useAsyncState<string>();
+
+  // Get
+  React.useEffect(() => {
+    SecureStore.getItemAsync(key).then((value) => {
+      setState(value);
+    });
+  }, [key, setState]);
+
+  // Set
+  const setValue = React.useCallback(
+    (value: string | null) => {
+      setState(value);
+      setStorageItemAsync(key, value);
+    },
+    [key, setState],
+  );
+
+  return [state, setValue];
+}
