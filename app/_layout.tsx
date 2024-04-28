3e9ce21ca2827ce91535ac649a59cfc3c57f1813
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-04-28 20:36:15 +0330

    feat: add new providers and match the navbar color with theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,33 @@
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { ThemeProvider } from "@/components";
+import { ThemeProvider, useAppTheme } from "@/components";
 
 export { ErrorBoundary } from "expo-router";
 
 const queryClient = new QueryClient({});
 
+const Stacks = () => {
+  const { colors } = useAppTheme();
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false, navigationBarColor: colors.elevation.level2 }} />
+    </Stack>
+  );
+};
+
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <Stack initialRouteName="(tabs)">
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <BottomSheetModalProvider>
+            <Stacks />
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
       </ThemeProvider>
     </QueryClientProvider>
   );
