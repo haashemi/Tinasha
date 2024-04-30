e9296efae945e7823c2c160cbc654a4dff91d495
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-04-30 22:54:45 +0330

    chore: update useStorageState to the way I like

--- components/useStorageState.ts
+++ components/useStorageState.ts
@@ -1,32 +1,19 @@
 import * as SecureStore from "expo-secure-store";
 import * as React from "react";
 
-type UseStateHook<T> = [[boolean, T | null], (value: T | null) => void];
-
-function useAsyncState<T>(initialValue: [boolean, T | null] = [true, null]): UseStateHook<T> {
-  return React.useReducer(
-    (state: [boolean, T | null], action: T | null = null): [boolean, T | null] => [false, action],
-    initialValue,
-  ) as UseStateHook<T>;
-}
+type UseStateHook<T> = [T | null, (value: T | null) => void];
 
 export async function setStorageItemAsync(key: string, value: string | null) {
-  if (value == null) {
-    await SecureStore.deleteItemAsync(key);
-  } else {
-    await SecureStore.setItemAsync(key, value);
-  }
+  if (value == null) return SecureStore.deleteItemAsync(key);
+  return await SecureStore.setItemAsync(key, value);
 }
 
 export function useStorageState(key: string): UseStateHook<string> {
-  // Public
-  const [state, setState] = useAsyncState<string>();
+  const [state, setState] = React.useState<string | null>(null);
 
   // Get
   React.useEffect(() => {
-    SecureStore.getItemAsync(key).then((value) => {
-      setState(value);
-    });
+    SecureStore.getItemAsync(key).then(setState);
   }, [key, setState]);
 
   // Set
