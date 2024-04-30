72015b7a6fbeb8cf0adcf4fb2e0e5eec6890a411
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-01 00:33:54 +0330

    feat: add MAL Auth API endpoints

---
+++ api/auth.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+const clientId = process.env.EXPO_PUBLIC_MAL_CLIENT_ID!;
+const redirectUri = "tinasha://sign-in";
+
+const tokenEndpoint = "https://myanimelist.net/v1/oauth2/token";
+
+export const makeKeyCode = (length: number) => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
+export const getAuthUrl = (state: string, codeChallenge: string) => {
+  const params = new URLSearchParams({
+    response_type: "code",
+    client_id: clientId,
+    state,
+    redirect_uri: redirectUri,
+    code_challenge: codeChallenge,
+    code_challenge_method: "plain",
+  });
+
+  return `https://myanimelist.net/v1/oauth2/authorize?${params.toString()}`;
+};
+
+export const exchangeCode = async ({ code, codeChallenge }: { code: string; codeChallenge: string }) => {
+  return axios.post(
+    tokenEndpoint,
+    {
+      client_id: clientId,
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeChallenge,
+    },
+    {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    },
+  );
+};
+
+export const refreshToken = async ({ refreshToken }: { refreshToken: string }) => {
+  return axios.post(
+    tokenEndpoint,
+    {
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
+    },
+    {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    },
+  );
+};
--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+export * from "./auth";
 export * from "./models";
 export * from "./useAnimeList";
 export * from "./useSeasonalAnime";
