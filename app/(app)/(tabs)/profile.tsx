e3fff280abd4254cad0b5be6866b23bddd399ac6
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-03 00:51:53 +0330

    feat: add placeholder profile tab

---
+++ app/(app)/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function ProfileTab() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Not Implemented</Text>
+    </View>
+  );
+}
