ffa09c07a02dbdf7993c6e5264236d45fc1a5508
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-03 02:04:06 +0330

    chore: use FlashList and AnimeListView for search route

--- app/(app)/search.tsx
+++ app/(app)/search.tsx
@@ -1,14 +1,14 @@
-import { Image } from "expo-image";
-import { useNavigation } from "expo-router";
+import { FlashList } from "@shopify/flash-list";
+import { Link, useNavigation } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
 import { useState } from "react";
 import { View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import { Searchbar, Surface, Text, TouchableRipple } from "react-native-paper";
+import { Searchbar, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useDebounce } from "use-debounce";
 
 import { useAnimeList } from "@/api";
+import { AnimeListView } from "@/components";
 
 export default function Search() {
   const safeArea = useSafeAreaInsets();
@@ -17,7 +17,10 @@ export default function Search() {
   const [searchQuery, setSearchQuery] = useState("");
   const [query] = useDebounce(searchQuery, 500);
 
-  const { data, isLoading, isError } = useAnimeList({ query });
+  const { data, isLoading, isError } = useAnimeList({
+    query,
+    fields: ["alternative_titles", "num_episodes", "mean", "my_list_status"],
+  });
 
   return (
     <>
@@ -31,17 +34,16 @@ export default function Search() {
         style={{ position: "absolute", zIndex: 1, top: safeArea.top + 10, marginHorizontal: 10 }}
       />
 
-      <FlatList
+      <FlashList
         data={data?.data}
         contentContainerStyle={{
-          gap: 10,
           paddingHorizontal: 10,
           paddingTop: safeArea.top + 80,
           paddingBottom: safeArea.bottom + 20,
         }}
-        keyExtractor={(item) => item.node.id.toString()}
+        estimatedItemSize={30}
         ListEmptyComponent={() => (
-          <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <View style={{ height: 200, justifyContent: "center", alignItems: "center" }}>
             <Text>
               {query === ""
                 ? "Search for something"
@@ -53,29 +55,21 @@ export default function Search() {
             </Text>
           </View>
         )}
+        keyExtractor={(item) => item.node.id.toString()}
         renderItem={({ item: { node } }) => (
-          <TouchableRipple
-            key={node.id.toString()}
-            borderless
-            onLongPress={async () => {
-              await WebBrowser.openBrowserAsync(`https://myanimelist.net/anime/${node.id}`);
-            }}
-            style={{ borderRadius: 10, overflow: "hidden", flex: 1 }}
-          >
-            <Surface mode="flat" style={{ height: 100, flexDirection: "row" }}>
-              <Image
-                style={{ height: 100, width: 100 }}
-                source={node.main_picture ? node.main_picture.large || node.main_picture.medium : null}
-                contentFit="cover"
-                transition={250}
-              />
-              <View>
-                <Text numberOfLines={2} ellipsizeMode="tail">
-                  {node.title}
-                </Text>
-              </View>
-            </Surface>
-          </TouchableRipple>
+          <Link asChild href={`/anime/${node.id}`}>
+            <AnimeListView
+              title={node.title}
+              status={node.my_list_status?.status}
+              score={node.my_list_status?.score}
+              meanScore={node.mean}
+              totalEpisodes={node.num_episodes}
+              style={{ margin: 5 }}
+              watchedEpisodes={node.my_list_status?.num_episodes_watched}
+              imageSrc={node.main_picture?.large ?? node.main_picture?.medium}
+              onLongPress={async () => WebBrowser.openBrowserAsync(`https://myanimelist.net/anime/${node.id}`)}
+            />
+          </Link>
         )}
       />
     </>
