ac087cd059a5a9fa68ea68ef1a33b83775f0ff9c
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-02 23:06:15 +0330

    chore: create redirectUri with makeRedirectUri

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { makeRedirectUri } from "expo-auth-session";
 
 const clientId = process.env.EXPO_PUBLIC_MAL_CLIENT_ID!;
-const redirectUri = "tinasha://sign-in";
+const redirectUri = makeRedirectUri({ scheme: "tinasha", path: "sign-in" });
 
 const tokenEndpoint = "https://myanimelist.net/v1/oauth2/token";
 
