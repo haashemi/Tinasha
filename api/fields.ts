9e698355ed2e8a1dcbc97ec822c0c6a59b99e3ef
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-03 18:11:16 +0330

    chore: separate fields and default fields

---
+++ api/fields.ts
@@ -0,0 +1,38 @@
+export type Field =
+  | "id"
+  | "title"
+  | "main_picture"
+  | "alternative_titles"
+  | "start_date"
+  | "end_date"
+  | "synopsis"
+  | "mean"
+  | "rank"
+  | "popularity"
+  | "num_list_users"
+  | "num_scoring_users"
+  | "nsfw"
+  | "created_at"
+  | "updated_at"
+  | "media_type"
+  | "status"
+  | "genres"
+  | "my_list_status"
+  | "num_episodes"
+  | "start_season"
+  | "broadcast"
+  | "source"
+  | "average_episode_duration"
+  | "rating"
+  | "pictures"
+  | "background"
+  | "related_anime"
+  | "related_mangarecommendations"
+  | "studios"
+  | "statistics"
+  | "opening_themes"
+  | "ending_themes";
+
+export const DefaultAnimeListFields = ["alternative_titles", "num_episodes", "mean", "my_list_status"].join(",");
+export const DefaultSeasonalAnimeFields = ["start_season", "mean", "my_list_status"].join(",");
+export const DefaultUserAnimeListFields = ["alternative_titles", "num_episodes", "mean", "my_list_status"].join(",");
--- api/models.ts
+++ api/models.ts
@@ -1,38 +1,3 @@
-export type Field =
-  | "id"
-  | "title"
-  | "main_picture"
-  | "alternative_titles"
-  | "start_date"
-  | "end_date"
-  | "synopsis"
-  | "mean"
-  | "rank"
-  | "popularity"
-  | "num_list_users"
-  | "num_scoring_users"
-  | "nsfw"
-  | "created_at"
-  | "updated_at"
-  | "media_type"
-  | "status"
-  | "genres"
-  | "my_list_status"
-  | "num_episodes"
-  | "start_season"
-  | "broadcast"
-  | "source"
-  | "average_episode_duration"
-  | "rating"
-  | "pictures"
-  | "background"
-  | "related_anime"
-  | "related_mangarecommendations"
-  | "studios"
-  | "statistics"
-  | "opening_themes"
-  | "ending_themes";
-
 export enum Season {
   Winter = "winter",
   Spring = "spring",
