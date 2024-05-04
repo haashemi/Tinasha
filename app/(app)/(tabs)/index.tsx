2152b54ad2e2279cc26bd2536b8e5af89eb63bbc
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-04 20:57:57 +0330

    chore: almost full rewrite of SeasonTab

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,192 +1,158 @@
 import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
 import { FlashList } from "@shopify/flash-list";
-import { Image } from "expo-image";
 import { Link } from "expo-router";
-import * as WebBrowser from "expo-web-browser";
-import { useRef, useState } from "react";
-import { Pressable, View } from "react-native";
-import { Button, Chip, FAB, Icon, SegmentedButtons, Surface, Text, TouchableRipple } from "react-native-paper";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, FAB, Icon, SegmentedButtons, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { Season, getSeason, useSeasonalAnime } from "@/api";
-import { useAppTheme } from "@/components";
-import { getStatusColor } from "@/lib";
+import { AnimeNode, Season, SeasonalAnimeSort, getSeason, useSeasonalAnime } from "@/api";
+import { AnimeCardView, LazyLoader, useAppTheme } from "@/components";
 
-// TODO: totally rewrite this page.
-// TODO: use useInfiniteQuery instead of useQuery
-export default function SeasonTab() {
-  const [sort, setSort] = useState<"anime_score" | "anime_num_list_users">("anime_num_list_users");
+const seasonsList = [
+  { value: "winter", label: "Winter" },
+  { value: "spring", label: "Spring" },
+  { value: "summer", label: "Summer" },
+  { value: "fall", label: "Fall" },
+];
+
+const sortList = [
+  { value: "anime_score", label: "Sort by Score" },
+  { value: "anime_num_list_users", label: "Sort by Members" },
+];
+
+const Header = ({ season, year }: { season: Season; year: number }) => {
+  const safeArea = useSafeAreaInsets();
+
+  return (
+    <View style={[{ marginTop: safeArea.top }, Styles.headerView]}>
+      <Text variant="headlineSmall" style={Styles.headerText}>
+        {season.toLocaleUpperCase()} {year}
+      </Text>
+
+      <Link asChild href="/search">
+        <Button mode="outlined" icon="magnify" style={{ flex: 1 }} onPress={() => {}}>
+          Search
+        </Button>
+      </Link>
+    </View>
+  );
+};
+
+const Footer = ({ isLoading }: { isLoading: boolean }) => (
+  <View style={{ height: 90 }}>{isLoading ? <LazyLoader /> : null}</View>
+);
+
+const SeasonTab = () => {
+  const [sort, setSort] = useState<SeasonalAnimeSort>("anime_num_list_users");
   const [year, setYear] = useState(new Date().getFullYear());
   const [season, setSeason] = useState(getSeason(new Date().getMonth()));
 
-  const theme = useAppTheme();
+  const { colors } = useAppTheme();
   const safeArea = useSafeAreaInsets();
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const { data, isSuccess, isFetching, refetch } = useSeasonalAnime({ year, season, sort });
+  const filterSheet = useRef<BottomSheetModal>(null);
+  const { fetchNextPage, hasNextPage, data, isFetching, refetch } = useSeasonalAnime({ year, season, sort });
 
-  const items = isSuccess
-    ? data.data.filter(({ node }) => node.start_season!.year === year && node.start_season!.season === season)
-    : [];
+  const allItems = useMemo(
+    () =>
+      data?.pages
+        .flatMap((page) => page.data)
+        .filter(({ node }) => node.start_season!.year === year && node.start_season!.season === season),
+    [data?.pages, season, year],
+  );
+
+  const keyExtractor = useCallback(({ node }: { node: AnimeNode }, i: number) => `${i}-${node.id}`, []);
+
+  const showFilterSheet = () => filterSheet.current?.present();
 
   return (
-    <>
-      <FAB
-        icon="filter"
-        mode="flat"
-        disabled={!isSuccess}
-        loading={!isSuccess}
-        variant="secondary"
-        style={{ position: "absolute", margin: 16, right: 0, bottom: 0, zIndex: 50 }}
-        onPress={() => bottomSheetModalRef.current?.present()}
-      />
+    <View style={{ flex: 1 }}>
+      <FAB icon="filter" loading={isFetching} style={Styles.fab} onPress={showFilterSheet} />
 
       <BottomSheetModal
-        ref={bottomSheetModalRef}
+        ref={filterSheet}
         enableDynamicSizing
         stackBehavior="replace"
-        handleIndicatorStyle={{ backgroundColor: theme.colors.onSurface }}
-        backgroundStyle={{ backgroundColor: theme.colors.elevation.level3 }}
+        handleIndicatorStyle={{ backgroundColor: colors.onSurface }}
+        backgroundStyle={{ backgroundColor: colors.elevation.level3 }}
       >
         <BottomSheetView
-          style={{
-            backgroundColor: theme.colors.elevation.level3,
-            alignItems: "center",
-            justifyContent: "space-around",
-            paddingHorizontal: 30,
-            paddingBottom: safeArea.bottom,
-            gap: 30,
-          }}
+          style={[{ backgroundColor: colors.elevation.level3, paddingBottom: safeArea.bottom }, Styles.bottomSheetView]}
         >
-          <View
-            style={{
-              flexDirection: "row",
-              width: "100%",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginTop: 30,
-            }}
-          >
+          <View style={Styles.bottomSheetYearView}>
             <Button mode="outlined" onPress={() => setYear((y) => y - 1)}>
-              <Icon source="minus" size={20} color={theme.colors.primary} />
+              <Icon source="minus" size={20} color={colors.primary} />
             </Button>
-
             <Text variant="titleLarge">{year}</Text>
-
             <Button mode="outlined" onPress={() => setYear((y) => y + 1)}>
-              <Icon source="plus" size={20} color={theme.colors.primary} />
+              <Icon source="plus" size={20} color={colors.primary} />
             </Button>
           </View>
 
-          <SegmentedButtons
-            value={season}
-            onValueChange={(v) => setSeason(v as Season)}
-            buttons={[
-              { value: "winter", label: "Winter" },
-              { value: "spring", label: "Spring" },
-              { value: "summer", label: "Summer" },
-              { value: "fall", label: "Fall" },
-            ]}
-          />
+          <SegmentedButtons buttons={seasonsList} value={season} onValueChange={(v) => setSeason(v as Season)} />
+          <SegmentedButtons buttons={sortList} value={sort} onValueChange={(v) => setSort(v as SeasonalAnimeSort)} />
 
-          <SegmentedButtons
-            value={sort}
-            onValueChange={(v) => setSort(v as "anime_score" | "anime_num_list_users")}
-            buttons={[
-              { value: "anime_score", label: "Sort by Score" },
-              { value: "anime_num_list_users", label: "Sort by Members" },
-            ]}
-          />
-
-          <Button mode="contained" style={{ marginBottom: 30 }} onPress={() => bottomSheetModalRef.current?.close()}>
+          <Button mode="contained" style={{ marginBottom: 30 }} onPress={() => filterSheet.current?.close()}>
             Set Filters
           </Button>
         </BottomSheetView>
       </BottomSheetModal>
 
       <FlashList
-        data={items}
+        data={allItems}
         numColumns={3}
-        estimatedItemSize={30}
+        contentContainerStyle={{ paddingHorizontal: 10 }}
+        estimatedItemSize={60}
         refreshing={isFetching}
         onRefresh={refetch}
-        contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 90 }}
-        ListHeaderComponent={() => (
-          <View
-            style={{
-              flex: 1,
-              gap: 20,
-              marginTop: safeArea.top,
-              paddingTop: 15,
-              margin: 15,
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text variant="headlineSmall" style={{ flex: 1, fontStyle: "italic", fontWeight: "bold" }}>
-              {season.toLocaleUpperCase()} {year}
-            </Text>
-
-            <Link asChild href="/search">
-              <Button mode="outlined" icon="magnify" style={{ flex: 1 }} onPress={() => {}}>
-                Search
-              </Button>
-            </Link>
-          </View>
-        )}
-        keyExtractor={(item) => item.node.id.toString()}
+        onEndReached={hasNextPage ? fetchNextPage : undefined}
+        onEndReachedThreshold={0.5}
+        ListHeaderComponent={() => <Header season={season} year={year} />}
+        ListFooterComponent={() => <Footer isLoading={hasNextPage && isFetching} />}
+        keyExtractor={keyExtractor}
         renderItem={({ item: { node } }) => (
-          <Link asChild href={`/anime/details/${node.id}`}>
-            <TouchableRipple
-              borderless
-              onLongPress={async () => {
-                await WebBrowser.openBrowserAsync(`https://myanimelist.net/anime/${node.id}`);
-              }}
-              style={{ borderRadius: 10, overflow: "hidden", flex: 1 }}
-            >
-              <Surface
-                mode="flat"
-                elevation={2}
-                style={{ height: 260, margin: 5, borderRadius: 10, overflow: "hidden" }}
-              >
-                <Image
-                  transition={250}
-                  allowDownscaling
-                  cachePolicy="memory-disk"
-                  contentFit="fill"
-                  source={node.main_picture?.large}
-                  style={{ height: 170 }}
-                />
-
-                <View style={{ padding: 5, flex: 1, justifyContent: "space-between" }}>
-                  <Text variant="titleMedium" numberOfLines={2} ellipsizeMode="tail">
-                    {node.title}
-                  </Text>
-
-                  <View style={{ flexDirection: "row", gap: 5 }}>
-                    <Chip compact icon="star" style={{ flexGrow: 1 }}>
-                      {node.mean! || "N/A"}
-                    </Chip>
-                    <Link asChild href={`/anime/edit/${node.id}`}>
-                      <Pressable
-                        style={{
-                          borderRadius: 5,
-                          justifyContent: "center",
-                          alignItems: "center",
-                          padding: 5,
-                          backgroundColor: getStatusColor(node.my_list_status?.status),
-                        }}
-                      >
-                        <Icon size={20} color="white" source="list-status" />
-                      </Pressable>
-                    </Link>
-                  </View>
-                </View>
-              </Surface>
-            </TouchableRipple>
-          </Link>
+          <AnimeCardView
+            animeId={node.id}
+            title={node.title}
+            status={node.my_list_status?.status}
+            meanScore={node.mean}
+            imageSrc={node.main_picture?.large ?? node.main_picture?.medium}
+            mediaType={node.media_type}
+            style={{ margin: 5 }}
+          />
         )}
       />
-    </>
+    </View>
   );
-}
+};
+
+const Styles = StyleSheet.create({
+  headerView: {
+    flex: 1,
+    gap: 20,
+    paddingTop: 15,
+    margin: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerText: { flex: 1, fontStyle: "italic", fontWeight: "bold" },
+
+  fab: { position: "absolute", margin: 16, right: 0, bottom: 0, zIndex: 1 },
+
+  bottomSheetView: {
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 30,
+    gap: 30,
+  },
+  bottomSheetYearView: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 30,
+  },
+});
+
+export default SeasonTab;
