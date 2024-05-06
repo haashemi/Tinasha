18171203c3d067b59a9e418105b88f73ed3faf08
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-06 18:28:52 +0330

    chore: use an infiniteQuery for useAnimeList

--- api/useAnimeList.ts
+++ api/useAnimeList.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { DefaultAnimeListFields } from "./fields";
 import { AnimeNode } from "./models";
@@ -7,6 +7,7 @@ import { useAuthSession } from "@/components";
 
 interface AnimeListOptions {
   query: string;
+  limit?: number;
 }
 
 interface Response {
@@ -14,19 +15,23 @@ interface Response {
   paging: { next: string };
 }
 
-export const useAnimeList = ({ query }: AnimeListOptions) => {
+export const useAnimeList = (opts: AnimeListOptions) => {
+  const { query, limit = 24 } = opts;
   const { client } = useAuthSession();
 
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["anime-list", query],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       if (query === "") return {} as Response;
 
       const resp = await client.get("/anime", {
-        params: { q: query, limit: 50, offset: 0, fields: DefaultAnimeListFields },
+        params: { q: query, limit, offset: pageParam * limit, fields: DefaultAnimeListFields },
       });
 
       return resp.data as Response;
     },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) =>
+      lastPage.paging?.next ? lastPageParam + 1 : undefined,
   });
 };
