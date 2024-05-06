6d4fcd5934be05264397a2f73b78f5c8572051db
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-06 21:21:48 +0330

    feat: add useMyUserInformation

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 export * from "./models";
 export * from "./useAnimeList";
+export * from "./useMyUserInformation";
 export * from "./useSeasonalAnime";
 export * from "./useUserAnimeList";
---
+++ api/useMyUserInformation.ts
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { useAuthSession } from "@/components";
+
+interface Response {
+  id: number;
+  name: string;
+  picture: string;
+  gender: string | null;
+  birthday: Date | null;
+  location: string | null;
+  joined_at: Date;
+  anime_statistics: {
+    num_items_watching: number;
+    num_items_completed: number;
+    num_items_on_hold: number;
+    num_items_dropped: number;
+    num_items_plan_to_watch: number;
+    num_items: number;
+    num_days_watched: number;
+    num_days_watching: number;
+    num_days_completed: number;
+    num_days_on_hold: number;
+    num_days_dropped: number;
+    /**num_watching_days + num_completed_days + num_on_hold_days + num_dropped_days */
+    num_days: number;
+    num_episodes: number;
+    num_times_rewatched: number;
+    mean_score: number;
+  } | null;
+  /**for example: "America/Los_Angeles" */
+  time_zone: string | null;
+  is_supporter: boolean | null;
+}
+
+export const useMyUserInformation = () => {
+  const { client } = useAuthSession();
+
+  return useQuery({
+    queryKey: ["my-user-information"],
+    queryFn: async () => {
+      const resp = await client.get("/users/@me", { params: { fields: "anime_statistics,time_zone" } });
+      return resp.data as Response;
+    },
+  });
+};
