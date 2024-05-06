037dfc3baea2823e69effafcbce38eff65660140
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-06 21:22:06 +0330

    feat: implement a basic profile tab for now

--- app/(app)/(tabs)/profile.tsx
+++ app/(app)/(tabs)/profile.tsx
@@ -1,10 +1,48 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { format } from "date-fns";
+import { Image } from "expo-image";
+import { ScrollView, View } from "react-native";
+import { Divider, Icon, Text } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { useMyUserInformation } from "@/api";
+import { useAppTheme } from "@/components";
 
 export default function ProfileTab() {
+  const theme = useAppTheme();
+  const safeArea = useSafeAreaInsets();
+  const { data } = useMyUserInformation();
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Not Implemented</Text>
-    </View>
+    <ScrollView style={{ paddingTop: safeArea.top }}>
+      <View style={{ padding: 30, flexDirection: "row" }}>
+        <Image style={{ width: 150, height: 200, borderRadius: theme.roundness * 4 }} source={data?.picture} />
+
+        <View style={{ justifyContent: "space-around", padding: 20 }}>
+          <Text numberOfLines={1} adjustsFontSizeToFit variant="headlineMedium">
+            {data?.name}
+          </Text>
+
+          {data?.birthday ? (
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <Icon size={20} source="cake-variant" />
+              <Text variant="bodyLarge">{format(data.joined_at, "MMMM do, yyyy")}</Text>
+            </View>
+          ) : null}
+
+          {data?.joined_at ? (
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <Icon size={20} source="calendar-check-outline" />
+              <Text variant="bodyLarge">{format(data.joined_at, "MMMM do, yyyy")}</Text>
+            </View>
+          ) : null}
+        </View>
+      </View>
+
+      <Divider />
+
+      {/* TODO: Add Statistics */}
+
+      {/* TODO: Add Debug button */}
+    </ScrollView>
   );
 }
