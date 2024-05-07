42a20f1f4bc3ded4f82007753d3ed30b58763d2a
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-08 02:04:14 +0330

    feat: add Tinasha SVG logo

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,7 @@ export { default as AnimeCardView } from "./AnimeCardView";
 export { default as AnimeCharactersView } from "./AnimeCharactersView";
 export { default as AnimeListView } from "./AnimeListView";
 export { default as LoadingView } from "./LoadingView";
+export * from "./Tinasha";
 export { default as TitledText } from "./TitledText";
 export * from "./hooks";
 export * from "./providers";
