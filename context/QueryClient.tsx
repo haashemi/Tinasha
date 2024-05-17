a71156ddbcd1639a2c548423dda1008fd165c453
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-17 23:48:13 +0330

    chore: hopefully fix of undefined error

--- context/QueryClient.tsx
+++ context/QueryClient.tsx
@@ -1,21 +1,23 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import { QueryClient } from "@tanstack/react-query";
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import type { ReactNode } from "react";
+import { PersistQueryClientProvider, removeOldestQuery } from "@tanstack/react-query-persist-client";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       gcTime: 1000 * 60 * 60 * 24, // 24 hours
-      staleTime: 1_800_000, // 30 minutes
     },
   },
 });
 
-const asyncStoragePersister = createAsyncStoragePersister({ storage: AsyncStorage });
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+  retry: removeOldestQuery,
+  throttleTime: 1_000,
+});
 
-export const QueryClientProvider = ({ children }: { children: ReactNode }) => (
+export const QueryClientProvider = ({ children }: { children: React.ReactNode }) => (
   <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: asyncStoragePersister }}>
     {children}
   </PersistQueryClientProvider>
